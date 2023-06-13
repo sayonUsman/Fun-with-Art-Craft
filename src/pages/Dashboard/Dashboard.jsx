966491590e53bc7bf830fdb7986c01979363e6cd
Sayon Usman <sayonusman.ma@gmail.com>
2023-06-13 12:25:35 +0300

    Added handleDelete method in dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,9 @@
 import useAllDetails from "../../hooks/useAllDetails";
 import useConfirmedClasses from "../../hooks/useConfirmedClasses";
+import Swal from "sweetalert2";
 
 const Dashboard = () => {
-  const [confirmedClasses] = useConfirmedClasses();
+  const [confirmedClasses, refetch] = useConfirmedClasses();
   const [allDetails] = useAllDetails();
   let confirmedClassesDetails = [];
   let idList = [];
@@ -21,6 +22,35 @@ const Dashboard = () => {
       confirmedClassDetails,
     ];
   });
+
+  const handleDelete = (classId) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Do you want to delete your selected class?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/confirmedClasses/${classId}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.acknowledged) {
+              refetch(); // To remove the class from the navbar cart
+              Swal.fire(
+                "Deleted!",
+                "The class has been deleted from your selected class.",
+                "success"
+              );
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div className="mt-20 md:mt-4 mb-7">
@@ -42,13 +72,22 @@ const Dashboard = () => {
 
           {confirmedClassesDetails.map((details) => (
             <tbody key={details?._id}>
-              <tr className="hover text-center">
+              <tr className="text-center">
                 <th>#</th>
                 <td>{details?.className}</td>
                 <td>{details?.availableSeats}</td>
                 <td>{`$${details?.price}.00`}</td>
-                <td>Pay Now</td>
-                <td>Delete</td>
+                <td>
+                  <button className="btn btn-link">Pay Now</button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-xs bg-red-400"
+                    onClick={() => handleDelete(details?._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             </tbody>
           ))}
